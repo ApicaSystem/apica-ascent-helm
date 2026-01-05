5b353d1dd8a7c335b56cf8c783065b71a3f73938
Eric Sproul <eric.sproul@apica.io>
2026-01-05 15:37:07 +0000

    Add a script to maintain onprem sample values
    Consumes the default values yaml, makes necessary modifications, then
    outputs the "t-shirt size" files. These will be directly linked from
    documentation to be the starting point for on-prem deployments.

---
+++ apica-ascent/update-onprem-values.pl
@@ -0,0 +1,90 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Storable qw(dclone);
+use YAML::PP;
+use YAML::PP::Common qw(:PRESERVE);
+
+my $input_file = 'values.yaml';
+
+my $ypp = YAML::PP->new(preserve => PRESERVE_ORDER | PRESERVE_FLOW_STYLE,
+                        boolean => 'JSON::PP',
+                        header => 0
+                        );
+my $yaml = $ypp->load_file($input_file);
+
+my @tshirts = qw(single small medium large);
+
+foreach my $size (@tshirts) {
+    # Defaults for small deployments
+    my $flash_replicas = 2;
+    my $flash_ml_replicas = 2;
+    my $flash_sync_replicas = 2;
+    my $flash_disc_replicas = 2;
+    my $coffee_server_replicas = 2;
+    my $coffee_worker_replicas = 2;
+    my $prometheus_replicas = 2;
+
+    if ($size eq 'medium') {
+        $flash_replicas = 4;
+        $coffee_worker_replicas = 3;
+    } elsif ($size eq 'large') {
+        $flash_replicas = 8;
+        $coffee_worker_replicas = 4;
+    }
+
+    my $clone = dclone($yaml); # deep clone to avoid modifying original
+
+    # Global bits
+    $clone->{global}{nodePort}{enabled} = $JSON::PP::false;
+    $clone->{global}{nodeSelectors}{enabled} = $JSON::PP::false;
+    $clone->{global}{persistence}{storageClass} = 'openebs-hostpath';
+
+    # Indicate which values should be set by the operator
+    $clone->{global}{domain}                             = '##YOUR_DOMAIN##';
+    $clone->{global}{environment}{postgres_password}     = '##YOUR_POSTGRES_PASSWORD##';
+    $clone->{global}{environment}{s3_url}                = '##YOUR_S3_URL##';
+    $clone->{global}{environment}{s3_access}             = '##YOUR_S3_ACCESS_KEY##';
+    $clone->{global}{environment}{s3_secret}             = '##YOUR_S3_ACCESS_SECRET##';
+    $clone->{global}{environment}{s3_bucket}             = '##YOUR_S3_BUCKET##';
+    $clone->{global}{environment}{s3_region}             = '##YOUR_S3_REGION##';
+    $clone->{global}{environment}{AWS_ACCESS_KEY_ID}     = '##YOUR_S3_ACCESS_KEY##';
+    $clone->{global}{environment}{AWS_SECRET_ACCESS_KEY} = '##YOUR_S3_ACCESS_KEY##';
+    $clone->{global}{environment}{awsServiceEndpoint}    = '##YOUR_S3_URL##';
+    $clone->{global}{environment}{admin_name}            = '##ADMIN_ACCOUNT_NAME##';
+    $clone->{global}{environment}{admin_password}        = '##ADMIN_ACCOUNT_PASSWORD##';
+    $clone->{global}{environment}{admin_org}             = '##ADMIN_ORGANIZATION##';
+    $clone->{global}{environment}{admin_email}           = '##ADMIN_EMAIL##';
+
+    # Kafka client always disabled
+    $clone->{'logiq-flash'}{kafka_client}{enabled} = $JSON::PP::false;
+
+    # Set secret names because we don't use Vault
+    $clone->{'logiq-flash'}{secrets_name} = 'my-ascent-ingest';
+    $clone->{gateway}{tls}{secretName} = 'my-ascent-ingress';
+
+    # Envoy proxy annotations not needed
+    delete $clone->{envoyGateway}{envoyProxy}{provider}{service}{annotations};
+
+    # Reduce memory limits for single-node
+    if ($size eq 'single') {
+        $clone->{postgres}{resources}{limits}{memory}               = '8000Mi';
+        $clone->{prometheus}{prometheus}{resources}{limits}{memory} = '2000Mi';
+        $clone->{redis}{master}{resources}{limits}{memory}          = '2000Mi';
+    }
+
+    $clone->{'logiq-flash'}{replicaCount}     = ($size eq 'single') ? 1 : $flash_replicas;
+    $clone->{'logiq-flash'}{replicaCountMl}   = ($size eq 'single') ? 1 : $flash_ml_replicas;
+    $clone->{'logiq-flash'}{replicaCountSync} = ($size eq 'single') ? 1 : $flash_sync_replicas;
+    $clone->{'flash-discovery'}{replicaCountDiscovery}    = ($size eq 'single') ? 1 : $flash_disc_replicas;
+    $clone->{'flash-coffee'}{coffee}{replicaCount}        = ($size eq 'single') ? 1 : $coffee_server_replicas;
+    $clone->{'flash-coffee'}{coffee_worker}{replicaCount} = ($size eq 'single') ? 1 : $coffee_worker_replicas;
+    $clone->{prometheus}{prometheus}{replicaCount}        = ($size eq 'single') ? 1 : $prometheus_replicas;
+
+    my $outfile = "values.$size.yaml";
+    $ypp->dump_file($outfile, $clone);
+
+    print "Updated $outfile\n";
+}
+
