cd04a40bceeacc768ee8bb3550e78be535d6e8c6
Eric Sproul <eric.sproul@apica.io>
2026-07-22 08:17:41 -0400

    SO-10035 Disable cnpg backups in sizing presets
    Causes failures on default setup because no S3 info is provided.
    
    Can be enabled by customer with their own S3 info.

--- apica-ascent/update-onprem-values.pl
+++ apica-ascent/update-onprem-values.pl
@@ -74,6 +74,9 @@ foreach my $size (@tshirts) {
     $clone->{'logiq-flash'}{secrets_name} = 'my-ascent-ingest';
     $clone->{gateway}{tls}{secretName} = 'my-ascent-ingress';
 
+    # Turn off cnpg backups by default, can be re-enabled if desired, and S3 settings added
+    $clone->{cnpg}{backups}{enabled} = $JSON::PP::false;
+
     # Envoy proxy annotations not needed
     delete $clone->{envoyGateway}{envoyProxy}{provider}{service}{annotations};
 
